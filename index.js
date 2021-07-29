8c52b9a96300696bc3715de851ec6ad75ac7c1be
Lisa <lisajmarks@pm.me>
2021-07-28 22:26:39 -0700

    JS counter project

---
+++ index.js
@@ -0,0 +1,14 @@
+//document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+let myAge = 32 
+let humanDogRatio = 7 
+let myDogage = myAge * humanDogRatio
+
+console.log(myDogage)
