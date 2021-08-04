aa1797d0896f6e473bdc48b36c1ceaeda654d578
Lisa <lisajmarks@pm.me>
2021-08-03 20:17:26 -0700

    save and reset count

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 //document.getElementById("count-el").innerText = 5
 
 //html get me the element "count-el"
-let countEl = document.getElementById("count-el");
+let countEl = document.getElementById("count-el")
 let count = 0
+let saveEl = document.getElementById("save-el")
 
 function increment() { 
-    count = count + 1 
-    countEl.innerText = count
-    console.log(count)
+    count += 1 
+    countEl.textContent = count
 }
 
 function save() { 
-    console.log(count)
+    let sCount = " " + count + " - "
+    saveEl.textContent += sCount
+    count = 0
+    countEl.textContent = count
 }
